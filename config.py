38ba69f18af3fc95feb213f490d6d34ea2b4ea77
I'm Sanji <filesharingbot78@gmail.com>
2024-07-20 09:24:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,99 @@
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","straw_hat_database")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://sagatobots00001:sagatobots100@cluster00001.vgdshkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00001")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/a79b5c323865b6760be36.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "straw_hat_bots") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031978775"))
+    
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
+
+
+class Txt(object):
+    # part of text configuration
+        
+    START_TXT = """Hello {} 
+    
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+    
+➻ Using This Bot You Can Auto Rename Of Your Files.
+    
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
+    
+➻ Use /tutorial Command To Know How To Use Me.
+    
+<b>Bot Is Made By @Straw_hat_bots</b>"""
+    
+    FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
+
+Use These Keywords To Setup Custom File Name
+
+✓ episode :- To Replace Episode Number
+✓ quality :- To Replace Video Resolution
+
+<b>➻ Example :</b> <code> /autorename Naruto Shippuden S02 - EPepisode - quality  [Dual Audio] - @Straw_hat_bots </code>
+
+<b>➻ Your Current Auto Rename Format :</b> <code>{format_template}</code> """
+    
+    ABOUT_TXT = f"""<b>🤖 My Name :</b> <a href='https://t.me/straw_hat_auto_rename_bot'>Straw Hat Auto Rename ⚡</a>
+<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
+<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
+<b>🚀 Server :</b> <a href='https://Koyeb.com'>Koyeb</a>
+<b>📢 Channel :</b> <a href='https://t.me/Straw_hat_bots'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐁ᴏᴛs</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/Urr_sanjii'>𝐒ᴀɴJɪ 𝐒αᴍᴀ</a>
+    
+<b>♻️ Bot Made By :</b> @Straw_hat_bots"""
+
+    
+    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+    
+⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....
+    
+⦿ /viewthumb - Use This Command To See Your Thumbnail
+⦿ /delthumb - Use This Command To Delete Your Thumbnail"""
+
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    
+⦿ /set_caption - Use This Command To Set Your Caption
+⦿ /see_caption - Use This Command To See Your Caption
+⦿ /del_caption - Use This Command To Delete Your Caption"""
+
+    PROGRESS_BAR = """\n
+<b>📁 Size</b> : {1} | {2}
+<b>⏳️ Done</b> : {0}%
+<b>🚀 Speed</b> : {3}/s
+<b>⏰️ ETA</b> : {4} """
+    
+    
+    DONATE_TXT = """<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+    
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+    
+<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> """
+    
+    HELP_TXT = """<b>Hey</b> {}
+    
+Here Is The Help For My Commands."""
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Developer @JishuDeveloper
+
